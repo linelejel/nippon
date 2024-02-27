589d0b79b30fe5a500667306a6a3fd138f04510e
linelejel <143701195+linelejel@users.noreply.github.com>
2024-02-27 09:10:37 +0100

    Add files via upload

---
+++ nippon/nippon.hjemmeside/javascript/script.js
@@ -0,0 +1,33 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', function () {
+    const filterButtons = document.querySelectorAll('#filter-buttons img');
+    const imageItems = document.querySelectorAll('.image-item');
+  
+    filterButtons.forEach(button => {
+      button.addEventListener('click', function () {
+        const filterValue = this.getAttribute('data-filter');
+  
+        // Skjul alle billeder
+        imageItems.forEach(item => {
+          item.style.display = 'none';
+        });
+  
+        // Vis kun billeder med mindst ét matchende filter
+        if (filterValue === 'all') {
+          imageItems.forEach(item => {
+            item.style.display = 'flex';
+          });
+        } else {
+          imageItems.forEach(item => {
+            const filters = item.getAttribute('data-filter').split(' ');
+  
+            if (filters.includes(filterValue)) {
+              item.style.display = 'flex';
+            }
+          });
+        }
+      });
+    });
+  });
+  
+  
